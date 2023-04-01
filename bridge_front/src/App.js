e2b5c2a248383e8c461ebc0969aebd3a694c2e3f
namsh1125 <namsh1125@naver.com>
2023-04-01 11:49:47 +0900

    Refactoring

--- bridge_front/src/App.js
+++ bridge_front/src/App.js
@@ -5,12 +5,12 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './components/home';
 import "./App.css"
+import Exercise from './exercise/Exercise';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Home/>
+      <Exercise />
     </div>
   );
 }
