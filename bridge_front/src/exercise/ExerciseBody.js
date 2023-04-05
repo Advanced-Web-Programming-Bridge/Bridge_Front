6320ce1958e5ed55586fc4ebc3471109f1620b86
namsh1125 <namsh1125@naver.com>
2023-04-05 15:32:28 +0900

    Change progress bar design

--- bridge_front/src/exercise/ExerciseBody.js
+++ bridge_front/src/exercise/ExerciseBody.js
@@ -1,45 +1,69 @@
-import ProgressBar from "react-animated-progress-bar";
 import { useState } from "react";
 import "./Exercise.css";
 import { ExerciseContext } from "./ExerciseContext";
 import TodayList from "./TodayList";
 import ExerciseCalendar from "./ExerciseCalendar";
+import styled from 'styled-components';
+import exercise from "../static/image/exercise.jpg";
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+const StyledBody = styled.div`
+  margin-left: 10%;
+  margin-right: 10%;
+  margin-top: 5vh;
+  width: 80%;
+`;
+
+const StyledBorder = styled.hr`
+  margin-left: 10%;
+  margin-right: 10%;
+  width: 80%;
+`;
+
+const StyledToday = styled.div`
+  display: flex;
+  height: 50vh;
+`;
+
+const StyledBody1 = styled.div`
+  width: 50%;
+  padding-top: 5%;
+  padding-left:3%;
+  padding-right:3%
+`;
+
+const StyledExerciseImage = styled.div`
+  background-image: url(${exercise});
+  background-size: cover;
+  margin-bottom: 10%;
+  width: 100%;
+  height: 60%;
+`;
 
 function ExerciseBody() {
   const [percent, setPercent] = useState(0);
   const [date, setDate] = useState(new Date());
 
   return (
-    <div className="body">
-      <div className="today">
-        {/* progress bar */}
-        <div className="progress-bar">
-          <ProgressBar
-            width="1"
-            height="1"
-            fontColor="white"
-            trackWidth="10"
-            percentage={percent}
-            trackPathColor="grey"
-            trackBorderColor="black"
-            hollowBackgroundColor="#333333"
-            defColor={{
-              fair: "orangered",
-              good: "yellow",
-              excellent: "green",
-              poor: "red",
-            }}
-          />
-        </div>
+    <StyledBody>
+      <h2>오늘 계획한 운동량입니다.</h2>
+      <StyledBorder />
+      <StyledToday>
 
+        {/* Image와 프로그래스바 */}
+        <StyledBody1>
+          <StyledExerciseImage />
+          <ProgressBar animated now={percent} />
+        </StyledBody1>
+        
         {/* 오늘 뭐 해야하는지 보여주는 화면. useContext를 활용해 하위 component에서 percent를 변경할 수 있도록 함 */}
         <ExerciseContext.Provider value={{ percent, setPercent }}>
           <TodayList />
         </ExerciseContext.Provider>
-      </div>
+      </StyledToday>
 
+      <StyledBorder />
       <div className="record">
-
         {/* calendar. useContext를 활용해 하위 component에서 click된 날자를 변경할 수 있도록 함 */}
         <div className="body-div3">
           <div className="calendar">
@@ -50,11 +74,9 @@ function ExerciseBody() {
         </div>
 
         {/* 해당 날짜에 뭐 해야하는지, 했는지 정보 */}
-        <div className="body-div4">
-          
-        </div>
+        <div className="body-div4"></div>
       </div>
-    </div>
+    </StyledBody>
   );
 }
 
